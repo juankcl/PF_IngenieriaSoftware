9dab529c05072128fa08a41e8fde6758e6cc596d
juankcl <esjuankarlos1@gmail.com>
2020-05-29 18:47:56 -0500

    Tienda

---
+++ Tienda/src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+	{
+		path: '',
+		redirectTo: 'folder/Inbox',
+		pathMatch: 'full'
+	},
+	{
+		path: 'folder/:id',
+		loadChildren: () => import('./folder/folder.module').then((m) => m.FolderPageModule)
+	},
+	{
+		path: 'buscar',
+		loadChildren: () => import('./pages/buscar/buscar.module').then((m) => m.BuscarPageModule)
+	},
+	{
+		path: 'cuenta',
+		loadChildren: () => import('./pages/cuenta/cuenta.module').then((m) => m.CuentaPageModule)
+	},
+	{
+		path: 'carrito',
+		loadChildren: () => import('./pages/carrito/carrito.module').then((m) => m.CarritoPageModule)
+	}
+];
+
+@NgModule({
+	imports: [ RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }) ],
+	exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
