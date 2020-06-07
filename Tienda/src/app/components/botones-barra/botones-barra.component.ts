7d9c5cae95be9b4c52e20e7510a96601e4bd7cbf
Emilio Puga <ea.pugaavalos@ugto.mx>
2020-06-06 22:37:57 -0500

    Agregar componente de barra de búsqueda y botones de carrito y perfil

---
+++ Tienda/src/app/components/botones-barra/botones-barra.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-botones-barra',
+  templateUrl: './botones-barra.component.html',
+  styleUrls: ['./botones-barra.component.scss'],
+})
+export class BotonesBarraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
