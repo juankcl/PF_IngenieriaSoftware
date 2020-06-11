7acdce8ad8f45f80bfe4e7ae7d79c840c8d3e5a2
juankcl <esjuankarlos1@gmail.com>
2020-06-11 15:05:49 -0500

    reaccion visual

--- Tienda/src/app/folder/folder.page.ts
+++ Tienda/src/app/folder/folder.page.ts
@@ -9,6 +9,48 @@ import { ActivatedRoute } from '@angular/router';
 export class FolderPage implements OnInit {
 	public folder: string;
 
+	cosas: Array<any> = [
+		{
+			titulo: 'NierAutomata',
+			color: '#444',
+			precio: 32,
+			img: '../../../assets/images/nier.jpg'
+		},
+		{
+			titulo: 'PlantVSZombie',
+			color: '#444',
+			precio: 22,
+			img: '../../../assets/images/planta.png'
+		},
+		{
+			titulo: 'Call of Duty',
+			color: '#444',
+			precio: 22,
+			img: '../../../assets/images/duty.jpg'
+		},
+		{
+			titulo: 'Sniper 3D',
+			color: '#444',
+			precio: 22,
+			img: '../../../assets/images/snip.jpg'
+		},
+		{
+			titulo: 'GTA 5',
+			color: '#444',
+			precio: 22,
+			img: '../../../assets/images/gta.jpg'
+		}
+	];
+
+	ofertas: Array<any> = [
+		{
+			titulo: 'Naruto',
+			color: '#999',
+			precio: 12,
+			img: '../../../assets/images/naruto.jpg'
+		}
+	];
+
 	constructor(private activatedRoute: ActivatedRoute) {}
 
 	ngOnInit() {
