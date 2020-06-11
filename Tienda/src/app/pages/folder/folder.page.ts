14a0914681221db88756364473cc363488bf051e
juankcl <esjuankarlos1@gmail.com>
2020-06-11 15:42:37 -0500

    Ahorita sirve xD

--- Tienda/src/app/pages/folder/folder.page.ts
+++ Tienda/src/app/pages/folder/folder.page.ts
@@ -13,18 +13,45 @@ import { MySQLApiService } from '../../services/my-sql-api.service';
 export class FolderPage implements OnInit {
 	productos: Producto[];
 
-	constructor(
-		private storageSer: StorageService,
-		private mySql: MySQLApiService
-	) { }
+	cosas: Array<any> = [
+		{
+			titulo: 'Perro',
+			color: '#444',
+			precio: 32,
+			img: '../../../assets/images/perro.jpg'
+		},
+		{
+			titulo: 'Gato',
+			color: '#999',
+			precio: 12,
+			img: '../../../assets/images/gato.jpg'
+		},
+		{
+			titulo: 'Costal Dog Chau 64kg',
+			color: '#999',
+			precio: 12,
+			img: '../../../assets/images/costal.jpg'
+		}
+	];
 
-	ngOnInit() { 
+	ofertas: Array<any> = [
+		{
+			titulo: 'Costal Dog Chau 32kg',
+			color: '#999',
+			precio: 12,
+			img: '../../../assets/images/costal.jpg'
+		}
+	];
+
+	constructor(private storageSer: StorageService, private mySql: MySQLApiService) {}
+	// f
+
+	ngOnInit() {
 		this.mySql.random().subscribe((response: Producto[]) => {
 			this.productos = response;
 			//console.log(this.productos);
 		});
 	}
-
 
 	ionViewWillEnter() {
 		this.mySql.random().subscribe((response: Producto[]) => {
